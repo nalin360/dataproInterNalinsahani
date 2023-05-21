e39210cd9c47b642f55dca11259752916ee4b8f3
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-21 15:56:50 +0530

    React Components Renderlist from itemlist Array

---
+++ src/components/Day1Assignment/Renderlist.js
@@ -0,0 +1,22 @@
+import React from 'react'
+const itemlist = [
+    'item 1',
+    'item 2 ',
+    'item 3',
+    'item 4',
+    'item 5'
+]
+ function Renderlist(content) {
+
+const listitems = itemlist.map((item,i) => {
+    return <li key={i}>{item}</li>
+});
+    return (
+    <div>
+        <h1>Render list</h1>
+        <ul>{} {listitems}</ul>
+    </div>
+  )
+}
+
+export default Renderlist;
