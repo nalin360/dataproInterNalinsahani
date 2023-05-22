fd34aa02c78bdb8337e00dc52cd1bfca672f5079
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-22 22:07:07 +0530

    usestate tasktracker,settasktracker , handleAddfunc => setTask new task upadate current state, listapperence toogle to change on click , deleteItem func => to del task and return

--- src/components/Day2Assignment/TaskTrackerApp.js
+++ src/components/Day2Assignment/TaskTrackerApp.js
@@ -1,66 +1,120 @@
 import React, { useState } from "react";
 
+import "../../assets/CSS/taskTrackerStyles.css";
+
 function TaskTrackerApp() {
-  const [taskname, setTaskName] = useState("");
-  const [taskDesc, setTaskDesc] = useState("");
+  //   const [taskname, setTaskName] = useState("");
+  //   const [taskDesc, setTaskDesc] = useState("");
   const [tasktraker, setTasktraker] = useState({
-    taskname : '',
-    taskDesc :''
+    taskname: "",
+    taskDesc: "",
   });
   const [tasks, setTasks] = useState([]); // createing usestate with initialstate []
 
-  const handleAdd = () => {
-    /**
-     *  The handleSubmit function updates the tasks array with a 
-     * new task object that contains the current values of the 
-     * taskname and taskDesc state variables.
-     */
-    setTasks([...tasks, { taskname, taskDesc }]);
-    setTaskName(""); // resets the values
-    setTaskDesc(""); // resets the values
-  };
- 
   /**
-   * * "Task Name" and "Task Description".
+   * * handleAdd function updates the tasks array
+   * * with the current value of the tasktraker object and
+   * * resets the values of the tasktraker object to empty strings.
    */
+  const handleAdd = () => {
+    // tasks state variable holds an object
+    // { ...tasks } spread syntax ensures that the state object is replaced rather than mutated.
+    setTasks([...tasks, tasktraker]);
+    setTasktraker({
+      taskname: "",
+      taskDesc: "",
+    });
+  };
+  const [litoggle, setlitoggle] = useState(false);
+  // using useState to toggle class
+  const listAppereance = () => {
+    setlitoggle(!litoggle);
+  };
+  // deleting a listitems
+
+  const deleteitems = (item) => {
+    setTasks(tasks.filter((task) => task.taskname !== item));
+    // update task state by calling setTask
+    // filter method to create a new array
+    // that includes only the tasks for which the callback function returns true
+  };
   return (
     <div>
       <h1>TaskTrackerApp</h1>
 
       {/* 2 input  */}
-      <div>
-        <label>Task Name </label>
-        <input
-          name={taskname}
-          type="email"
-          placeholder="Email"
-          onChange={(e) => {
-            setTaskName(e.target.value);
-          }}
-        />
-        <label>Task Description </label>
-        <textarea
-          name={taskDesc}
-          type="textarea"
-          placeholder="Task Description"
-          onChange={(e) => setTaskDesc(e.target.value)}
-        />
+      <div className="trackerForm">
+        <form>
+          {/* <label>Task Name </label> */}
+          <input
+            name={tasktraker.taskname}
+            type="name"
+            placeholder="Task Name"
+            required
+            onChange={(e) => {
+              setTasktraker({
+                ...tasktraker,
+                taskname: e.target.value,
+              });
+            }}
+          />
+          {/* <label>Task Description </label> */}
+          <textarea
+            name={tasktraker.taskDesc}
+            type="textarea"
+            placeholder="Task Description"
+            onChange={(e) => {
+              setTasktraker({
+                ...tasktraker,
+                taskDesc: e.target.value,
+              });
+            }}
+          />
 
-        <button type="submit" onClick={handleAdd}
-        
-        >ADD</button>
+          <button
+            type="submit"
+            onClick={handleAdd}
+            disabled={tasktraker.taskname === "" || tasktraker.taskDesc === ""}
+          >
+            ADD
+          </button>
+        </form>
       </div>
       {/* **********************************************************************/}
-      <div>
+      <div >
         {/* another div: contains ul>li */}
-        <div></div>
-        <ul>
-        {tasks.map((task) => (
-            <li key={task.taskname}>
-              {task.taskname}: {task.taskDesc}
-            </li>
+        {/* <div></div> */}
+        <div>
+          {tasks.map((task) => (
+            <div className="listitems" key={task.taskname}>
+              <div>
+                <ul>
+                  <li
+                    placeholder={task.taskname}
+                    className={litoggle ? "li-toggle" : ""}
+                    onClick={listAppereance}
+                  >
+                    <span>Task Name :</span> {task.taskname}<br/>
+                    <span>Task Description:</span>{task.taskDesc}
+                     
+                  </li>
+                </ul>
+              </div>
+              <div className="bttn">
+                <button
+                  placeholder="X"
+                  onClick={() => {
+                    deleteitems(task.taskname);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+
+              {/*  */}
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
