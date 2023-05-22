0079bbeb1105783b3809a05af3c1a83439e2312c
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-22 22:02:36 +0530

    imported Task TrackerApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Renderlist from './components/Day1Assignment/Renderlist';
+// import Renderlist from './components/Day1Assignment/Renderlist';
+import TaskTrackerApp from './components/Day2Assignment/TaskTrackerApp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Renderlist content="Hi! there"/>
+      {/* <Renderlist content="Hi! there"/> */}
+      <TaskTrackerApp />
     </div>
   );
 }
